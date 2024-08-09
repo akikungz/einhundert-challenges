8c42d3208ffe2742fcbf00af0c4ef965b4b48a2b
Thitipong Tapianthong <akikungz@proton.me>
2024-08-09 14:12:30 +0700

    feat: add problem no. 80 but no finish

---
+++ problems/80/librarybook_class_with_a_menu_interface.py
@@ -0,0 +1,12 @@
+class LibraryBook:
+    def __init__(self, title: str, author: str, year_published: int, isbn: str) -> None:
+        pass
+    
+    def check_out(self) -> None:
+        pass
+    
+    def check_in(self) -> None:
+        pass
+    
+    def display_info(self) -> str:
+        pass
